3e302c07b1952b41aed15bd916fe42a88fc31ab3
Nick Newman <nnewman5402@gmail.com>
2026-02-18 23:08:54 -0500

    Restore hub directory placeholders (.gitkeep) after removing Arduino sketches

--- firmware/hub/shared/protocol/rmt_link.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include "driver/rmt_types.h"
-
-rmt_symbol_word_t bit_symbol(bool bit);
-
-void send_frame18_rmt(uint32_t frame18); // uses tx_chan/copy_encoder inside .cpp
-
-bool decodeFrame18FromSymbols(const rmt_symbol_word_t* syms, int count, uint32_t* outFrame);
